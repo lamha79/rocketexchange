8ca1ad3fc75e56ff1a64ff56ed20b84e7fe2875f
PancakeSyrup <bond041688@gmail.com>
2023-06-05 22:19:00 +0700

    Change Frontend

--- frontend/packages/react-app/src/assets/index.js
+++
@@ -1,5 +0,0 @@
-import chevronDown from "./chevron-down.svg";
-import ethereumLogo from "./ethereumLogo.png";
-import rocketswapLogo from "./rocketswapLogo.png";
-
-export { chevronDown, ethereumLogo, rocketswapLogo };
--- frontend/packages/react-app/src/utils/getRouterInfo.js
+++
@@ -1,9 +0,0 @@
-import { abis } from "@rocketswap/contracts";
-
-export const getRouterInfo = async (routerAddress, web3) => {
-  const router = new web3.eth.Contract(abis.router02, routerAddress);
-  
-  return {
-    factory: await router.methods.factory().call(),
-  };
-}
