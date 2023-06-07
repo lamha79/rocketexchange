ceaeb5111caf01ac2cf09fa0ca3415791db02a21
donglm1605 <116880839+donglm1605@users.noreply.github.com>
2023-06-08 01:37:00 +0700

    Update Admin.jsx

--- swap/src/pages/Admin.jsx
+++ swap/src/pages/Admin.jsx
@@ -4,95 +4,63 @@ import { Container, Row, Col } from "reactstrap";
 import "../styles/swap.css";
 
 const Admin = () => {
-    const addressRef = useRef("");
-    const priceRef = useRef("");
-    
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-    };
-    useEffect(() => {
-        // 👇️ scroll to top on page load
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
-    }, [])
 
-    return (
+        const adressRef = useRef("");
+        const amountRef = useRef("");
+    
+        const handleSubmit = (e) => {
+        e.preventDefault();
+        };
+    
+        useEffect(() => {
+        // 👇️ scroll to top on page load
+        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+        }, []);
+    
+        return (
         <>
-        <CommonSection tittle={"Admin add collection"} />
-        <section>
+            <CommonSection tittle={"Admin"} />
+            <section>
             <Container>
-            <Row>
+                <Row>
                 <Col lg='6' md='6' className="m-auto text-center">
-                <h2>Please type address collection</h2>
-                <div className="contact mt-4">
+                    <h2>Please type address</h2>
+                    <div className="contact mt-4">
                     <form onSubmit={handleSubmit}>
-                    <div className="form_input">
+                        <div className="form_input">
                         <input 
                             type="text" 
-                            placeholder='Address Collection:' 
-                            ref={addressRef}
+                            placeholder='Type Address:' 
+                            ref={adressRef}
                         />
-                    </div>
-                    <button 
-                        className="send__btn" 
-                        style={{
-                            border:'none', 
-                            padding:'7px 25px', 
-                            borderRadius:'5px',
-                            marginTop:'20px',
-                        }}
-                    >
-                        Add Adress
-                    </button>
-                    </form>
-                </div>
-                </Col>
-                <Col lg='6' md='6' className="m-auto text-center">
-                <h2>Token</h2>
-                <div className="contact mt-4">
-                    <form onSubmit={handleSubmit}>
-                    <div className="row currencyInput">
-                                <div className="col-md-6 numberContainer">
-                                    <input
-                                        className="currencyInputField"
-                                        placeholder="0.0"
-                                    />
-                                </div>
-                                <div className="col-md-6 tokenContainer">
-                                    <span className="tokenName">LINK</span>
-                                    <div className="balanceContainer">
-                                        <span className="balanceAmount">
-                                            Balance:{" "}
-                                        </span>
+                        </div>
+    
+                        <div className="row currencyInput">
+                                    <div className="col-md-6 numberContainer">
+                                        <input
+                                            className="currencyInputField"
+                                            placeholder="0.0"
+                                        />
+                                    </div>
+                                    <div className="col-md-6 tokenContainer">
+                                        <span className="tokenName">LINK</span>
+                                        <div className="balanceContainer">
+                                            <span className="balanceAmount">
+                                                Balance:{" "}
+                                            </span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                    <button 
-                        className="send__btn" 
-                        style={{
-                            border:'none', 
-                            padding:'7px 25px', 
-                            borderRadius:'5px',
-                            marginTop:'20px',
-                        }}
-                    >
-                        Accept
-                    </button>
-                    </form>
-                </div>
-                </Col>
-                <Col lg='6' md='6' className="m-auto text-center">
-                <h2>Please add price</h2>
-                <div className="contact mt-4">
-                    <form onSubmit={handleSubmit}>
-                    <div className="form_input">
+    
+                        <div className="form_input">
                         <input 
                             type="text" 
-                            placeholder='Price collection:' 
-                            ref={priceRef}
+                            placeholder='Amount' 
+                            ref={amountRef}
                         />
-                    </div>
-                    <button 
+                        </div>
+    
+                        <button 
                         className="send__btn" 
                         style={{
                             border:'none', 
@@ -100,28 +68,17 @@ const Admin = () => {
                             borderRadius:'5px',
                             marginTop:'20px',
                         }}
-                    >
-                        Accept
-                    </button>
+                        >
+                        Confirm
+                        </button>
                     </form>
-                </div>
-                <button 
-                        className="send__btn" 
-                        style={{
-                            border:'none', 
-                            padding:'7px 25px', 
-                            borderRadius:'5px',
-                            marginTop:'20px',
-                        }}
-                    >
-                        Add Collection
-                    </button>
+                    </div>
                 </Col>
-            </Row>
+                </Row>
             </Container>
-        </section>
+            </section>
         </>
-    );
+        );
     };
-
-export default Admin;
+    
+    export default Admin;
